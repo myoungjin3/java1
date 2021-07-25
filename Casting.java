258553f94a28204d40a9b5e303e2858e3549ea98
myoungjin3 <smjsih@naver,com>
2021-07-26 02:11:49 +0900

    데이터 타입의 변환

---
+++ Casting.java
@@ -0,0 +1,26 @@
+package Data_and_operation.bin;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		
+		double a = 1.1;
+		double b = 1;
+		double b2 = (double) 1;
+		System.out.println(b);
+		
+//		int c = 1.1;
+		double d = 1.1;
+		int e = (int) 1.1;
+		System.out.println(e);
+		
+		// 1 to String
+		String f = Integer.toString(1);
+		System.out.println(f.getClass()); 
+		
+		﻿
+	
+		﻿
+	}
+
+}
